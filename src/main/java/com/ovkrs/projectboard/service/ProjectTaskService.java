0cd4a30bfdf8232bcb766ed9d961d47763add00c
RavinduSamaranayake <ovkravindu95@gmail.com>
2019-06-02 17:37:36 +0530

    create post request successfully to save projectTasks

--- src/main/java/com/ovkrs/projectboard/service/ProjectTaskService.java
+++ src/main/java/com/ovkrs/projectboard/service/ProjectTaskService.java
@@ -1,5 +1,6 @@
 package com.ovkrs.projectboard.service;
 
+import com.ovkrs.projectboard.domain.ProjectTask;
 import com.ovkrs.projectboard.repository.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,16 @@ public class ProjectTaskService {
 
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
+
+    public ProjectTask saveOrUpdateProjectTask(ProjectTask projectTask){
+
+        if(projectTask.getStatus()== null || projectTask.getStatus()== ""){
+            projectTask.setStatus("TO_DO");
+        }
+
+        return projectTaskRepository.save(projectTask);
+
+    }
+
+
 }
--- src/main/java/com/ovkrs/projectboard/web/ProjectTaskController.java
+++ src/main/java/com/ovkrs/projectboard/web/ProjectTaskController.java
@@ -1,16 +1,23 @@
 package com.ovkrs.projectboard.web;
 
+import com.ovkrs.projectboard.domain.ProjectTask;
 import com.ovkrs.projectboard.service.ProjectTaskService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/board")
 @CrossOrigin
 public class ProjectTaskController {
    @Autowired
+   private ProjectTaskService projectTaskService;
 
-    private ProjectTaskService projectTaskService;
+   @PostMapping("")
+   public ResponseEntity<?> addPTToBoard(@RequestBody ProjectTask projectTask){
+
+       ProjectTask newPT = projectTaskService.saveOrUpdateProjectTask(projectTask); // save the request projectTask in db
+       return new ResponseEntity<ProjectTask>(newPT, HttpStatus.CREATED);
+   }
 }
